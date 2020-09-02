8b69106ac81e1b710f0b556c74674f0d74600b5a
Haleydu <924323178@qq.com>
2020-09-02 11:46:27 +0800

    md5\versionname\versionCode

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,9 +2,22 @@
 
 set -e
 
+printf "{"
+printf "\n"
 MD5=$(md5sum app/build/outputs/apk/release/PlayVideo*.apk)
+VERSIONNAME=$(git describe --tags)
 
+printf "versionCode:"
+printf $TRAVIS_BUILD_NUMBER
+printf "\n"
+
+printf "versionName:"
+printf $VERSIONNAME
+printf "\n"
+
+printf "md5:"
 printf $MD5
 printf "\n"
+printf "}"
 
 
