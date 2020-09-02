cddf40df586381dca942f777af65d2de45bf8de9
Haleydu <924323178@qq.com>
2020-09-02 11:30:20 +0800

    update

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,27 +2,9 @@
 
 set -e
 
-TOOLS="$(ls -d ${ANDROID_HOME}/build-tools/* | tail -1)"
+MD5=$(md5sum app/build/outputs/apk/release/PlayVideo*.apk)
 
-printf $TOOLS
+printf $MD5
 printf "\n"
-
-mkdir -p repo/apk
-mkdir -p repo/icon
-
-cp -f apk/* repo/apk
-
-cd repo
-
-APKS=( ../apk/*".apk"* )
-
-for APK in ${APKS[@]}; do
-  
-    printf APKS
-    MD5=$(md5sum APK)
-    printf $MD5
-    printf "\n"
-
-done
 
 
