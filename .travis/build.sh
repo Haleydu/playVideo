c0e7a5c5308719d86ec1288f517712b9fc628d1f
Haleydu <924323178@qq.com>
2020-09-02 12:05:47 +0800

    test jq

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 set -e
-
-printf "{"
-printf "\n"
 MD5=$(md5sum app/build/outputs/apk/release/PlayVideo*.apk)
 VERSIONNAME=$(git describe --tags)
 
+printf "{"
+printf "\n"
 printf "versionCode:"
 printf $TRAVIS_BUILD_NUMBER
 printf "\n"
-
 printf "versionName:"
 printf $VERSIONNAME
 printf "\n"
-
 printf "md5:"
 printf $MD5
 printf "\n"
 printf "}"
 
+jq -n \
+        --argjson versionCode "$VERSIONNAME" \
+        --arg versionName "$VERSIONNAME" \
+        --arg md5 $MD5 \
+        '{versionCode:$versionCode, versionName:$versionName, md5:$md5}'
 
+jq -sr '[.[]]' > index.json
+
+cat index.json
+
+pwd
