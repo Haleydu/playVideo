052847220e111ad92a1b39c5d3a6064df4c093f0
Haleydu <924323178@qq.com>
2020-09-02 17:32:56 +0800

    update

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,23 +3,26 @@
 set -e
 MD5=$(md5sum app/build/outputs/apk/release/PlayVideo*.apk)
 VERSIONNAME=$(git describe --tags)
-
+printf "\n"
 printf "{"
 printf "\n"
-printf "versionCode:"
+printf "  versionCode:"
 printf $TRAVIS_BUILD_NUMBER
 printf "\n"
-printf "versionName:"
+printf "  versionName:"
 printf $VERSIONNAME
 printf "\n"
-printf "md5:"
+printf "  md5:"
 printf $MD5
 printf "\n"
 printf "}"
+printf "\n"
 
-echo versionCode:$TRAVIS_BUILD_NUMBER > index.json
-echo versionName:"$VERSIONNAME" > index.json
-echo md5:"$MD5" > index.json
-cat index.json
+echo "{
+  versionCode:\"$TRAVIS_BUILD_NUMBER\"
+  versionName:\"$VERSIONNAME\"
+  md5:\"$MD5\"
+}" > ApkInfo.json
 
+printf "\n"
 pwd
